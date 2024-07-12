033ccf355bb4d254950ab4195224d0086a8cf3b6
PrantaHossen <prantahossen@gmail.com>
2024-07-13 00:48:41 +0600

    Using fakerPHP insert value in the database

---
+++ DB_Insert/database/seeders/StudentSeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\student;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\File;
+
+class StudentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+
+        for($i=0;$i<10;$i++)
+        {
+            student::create([
+                'name'=>fake()->name(),
+                'email'=>fake()->unique()->email()
+            ]);
+        }
+
+
+
+        /* $json = File::get(path:'/database/json/students.json');
+            $students=collect(json_decode($json));
+
+            $students->each(function($student)
+            {
+                student::create([
+                    'name'=>$student->name,
+                    'email'=>$student->email
+                ]);
+
+            }); */
+        /*  student::Create([
+            'name'=>'Pranta Hossen',
+            'email'=>'prantahossen@gmail.com',
+            'cgpa'=>3.80
+        ]); */
+}
+}
